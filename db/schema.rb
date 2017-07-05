ffc9833a310d06027bfe188868bb8164c48c0358
Nathan <lyon.nathan@gmail.com>
2017-07-05 23:24:07 +0000

    password field in user table changed to encrypted_password since the password will not be stored in plain text

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20170705225515) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "encrypted_password"
     t.string "preferred_name"
     t.string "phone_number"
     t.datetime "created_at", null: false
