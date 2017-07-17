f5505cb1de3b3c6d237e483082e7276fa81cdd59
Nathan <lyon.nathan@gmail.com>
2017-07-17 00:52:49 +0000

    Secure Login Currently Working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'create_account/new'
   post 'create_account/create'
-
-  get 'welcome/index'
-
+  post 'welcome/login_attempt'
+  match 'login', to: 'welcome#index', via: 'get'
+  #get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
