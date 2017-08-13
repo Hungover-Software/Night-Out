1b6a5a14121e29ea9814c701482f9d88fca0612b
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 18:47:47 -0500

    Update fp_controller.rb

--- app/controllers/fp_controller.rb
+++ app/controllers/fp_controller.rb
@@ -1,4 +1,17 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Forgot Password Controller (FpController)
+#
+# Authors: 
+
 class FpController < ApplicationController
+  
+  # Method forgot_password
+  # Parameters:
+  #
+  # Output:
   def forgot_password
     
   end
