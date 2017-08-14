6638a1105c564a4afaaf83df9828a129dd6adda2
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 20:19:28 -0500

    added comments for rdoc

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Class: Event Model (Event)
+#
+# Authors:
+
 class Event < ApplicationRecord
   belongs_to :user
   has_many :event_invites, dependent: :destroy
