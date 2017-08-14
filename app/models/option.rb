d6120b0a1de1ef731d8a13a29703fdabb5120029
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 20:30:47 -0500

    Update option.rb

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,11 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Class: Option Model (Option)
+#
+# Authors:
+
 class Option < ApplicationRecord
   belongs_to :user
   belongs_to :poll
