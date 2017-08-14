0357a375f266d509a03224e6f1bcd6c57f4e10f3
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 20:17:35 -0500

    added comments for rdoc

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Application Record Model (ApplicationRecord)
+#
+# Authors: 
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
