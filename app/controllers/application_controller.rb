d938fa09c6fb3a966944dce3b59d302a66f69c64
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 19:48:03 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Account Creation Controller (CreateAccountController)
+# Controls the actions for the createaccount/new.html.erb page
+# createaccount/new.html.erb is used to signup for a new account with the Night Out app.
+# Include: SessionsHelper
+# Authors: Nathan Lyon,
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   protected
+  
+  # Method authenticate_user
+  # Parameters:
+  #
+  # Output:
   def authenticate_user
   	unless session[:user_id]
   		redirect_to(:controller => 'sessions', :action => 'login')
@@ -12,8 +27,11 @@ class ApplicationController < ActionController::Base
   		return true
   	end
   end
-
-  #This method for prevent user to access Signup & Login Page without logout
+  
+  # Method save_login_state
+  # Parameters: None
+  # The save_login_state method prevents users from accessing the Signup & Login Page without logout
+  # Output: boolean true or false
   def save_login_state
     if session[:user_id]
             redirect_to(:controller => 'sessions', :action => 'home')
