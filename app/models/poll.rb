4c2345fded786f88c9e7a56bb4c989693c2446e1
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 20:31:35 -0500

    added comments for rdoc

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,11 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Class: Poll Model (Poll)
+#
+# Authors:
+
 class Poll < ApplicationRecord
   belongs_to :event
   has_many :options, dependent: :destroy
