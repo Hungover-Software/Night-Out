186bdfb5ac4b33a0ed0c78e4b0c604ffcf2f1d0c
UNOMAHANLYON <nlyon@unomaha.edu>
2017-08-13 20:37:21 -0500

    added comments for rdoc

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,11 @@
+# Night Out event planning application
+# Hungover Software
+# University of Nebraska Omaha
+# CSCI-4830-860 SU17
+# Class: Vote Model (Vote)
+#
+# Authors:
+
 class Vote < ApplicationRecord
   belongs_to :option
   belongs_to :user
